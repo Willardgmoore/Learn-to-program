79241bc96c7e54ede747b21e2fab97311f01fcd6
Willard Moore <will@thefixitgeek.com>
2015-05-19 13:35:16 -0700

    Progress
    Progress

--- 10.5.3.rb
+++ 10.5.3.rb
@@ -1,9 +1,16 @@
 puts "We're going to sing 99 bottles of beer on the wall."
 puts "How many bottles would you like up there this time?"
 
-bottles = ''
+bottles = gets.chomp
+
 require './english_number.rb'
 #num_string?(bottles)
 
-
+puts
 puts "#{bottles} bottles of beer on the wall"
+
+while bottles > 0 
+puts "#{bottles} bottles of beer on the wall"
+
+bottles -= 1
+end
